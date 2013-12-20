3ec30b06b9a37a73e214c111cfe701451b7031e0
Eduardo Cuomo <eduardo.cuomo.ar@gmail.com>
2013-12-20 16:02:12 -0200

    Update xpopup.js

--- xpopup.js
+++ xpopup.js
@@ -1,41 +1,44 @@
 /**
- * Open a PopUp Window.
- * Copyright (c) 2013 XPOPUP | Eduardo Daniel Cuomo | eduardo.cuomo.ar@gmail.com
- *
- * By: Edueado Daniel Cuomo.
- *
- * Project: https://github.com/reduardo7/xpopup
- *
- * @param p {Object} Options:
- *  url {String} OPTIONAL. URL to load in PopUp.
- *  data {Object} OPTIONAL. URL params. If "url" is not used, this option is not used.
- *  name {Stirng} OPTIONAL. Window name.
- *  fullscreen {Boolean} OPTIONAL. DEFAULT: false. Open PopUp as full screen.
- *  toolbar {Boolean} OPTIONAL. DEFAULT: false. Show toolbar in PopUp?
- *  location {Boolean} OPTIONAL. Default: false. Lock location bar?
- *  status {String} OPTIONAL. Default: "". Text in status bar.
- *  menubar {Boolean} OPTIONAL. Default: false. Show menu bar?
- *  scrollbars {Boolean} OPTIONAL. Default: true. Show scroll bars?
- *  resizable {Boolean} OPTIONAL. Default: true. Allow resize window?
- *  width {Integer} OPTIONAL. Default: 500. Window width.
- *  height {Integer} OPTIONAL. Default: 400. Window height.
- *  left {Integer} OPTIONAL. Window left position. Overwrites "center" option.
- *  top {Integer} OPTIONAL. Window top position. Overwrites "center" option.
- *  center {Boolean} OPTIONAL. Default: true. Center window in screen?
- *  close {Function} OPTIONAL. Callback function on close window event.
- *  modal {Boolean} OPTIONAL. Default: false. Auto set focus and block UI?
- * @return Object.
- *  win: Opened PopUp window object.
- *  center(): Center PopUp on the screen. Returns false on error.
- *  maximize(): Maximize PopUp. Returns false on error.
- *  focus(): Focus on PopUp window.
- *  closed(): Returns TRUE if PopUp window is closed.
- */
-var xpopup = function(p) {
-  var d = function(v) {
-		return (typeof(v) != 'undefined') && (v != null);
+* Open a PopUp Window.
+* Copyright (c) 2013 XPOPUP | Eduardo Daniel Cuomo | eduardo.cuomo.ar@gmail.com
+*
+* By: Edueado Daniel Cuomo.
+*
+* Project: https://github.com/reduardo7/xpopup
+*
+* @param p {Object} Options:
+*  url {String} OPTIONAL. URL to load in PopUp.
+*  data {Object} OPTIONAL. URL params. If "url" is not used, this option is not used.
+*  name {Stirng} OPTIONAL. Window name.
+*  fullscreen {Boolean} OPTIONAL. DEFAULT: false. Open PopUp as full screen.
+*  toolbar {Boolean} OPTIONAL. DEFAULT: false. Show toolbar in PopUp?
+*  location {Boolean} OPTIONAL. Default: false. Lock location bar?
+*  status {String} OPTIONAL. Default: "". Text in status bar.
+*  menubar {Boolean} OPTIONAL. Default: false. Show menu bar?
+*  scrollbars {Boolean} OPTIONAL. Default: true. Show scroll bars?
+*  resizable {Boolean} OPTIONAL. Default: true. Allow resize window?
+*  width {Integer} OPTIONAL. Default: 500. Window width.
+*  height {Integer} OPTIONAL. Default: 400. Window height.
+*  left {Integer} OPTIONAL. Window left position. Overwrites "center" option.
+*  top {Integer} OPTIONAL. Window top position. Overwrites "center" option.
+*  center {Boolean} OPTIONAL. Default: true. Center window in screen?
+*  position {string} OPTIONAL. Default: "". Set PopUp position.
+*      Parent: screen|window (Default: window).
+*      Postitions: top, left, bottom, right, middle (Y), center (X).
+*  close {Function} OPTIONAL. Callback function on close window event.
+*  modal {Boolean} OPTIONAL. Default: false. Auto set focus and block UI?
+* @return Object.
+*  win: Opened PopUp window object.
+*  center(): Center PopUp on the screen. Returns false on error.
+*  maximize(): Maximize PopUp. Returns false on error.
+*  focus(): Focus on PopUp window.
+*  closed(): Returns TRUE if PopUp window is closed.
+*/
+var xpopup = function (p) {
+	var d = function (v) {
+		return (typeof (v) != 'undefined') && (v != null);
 	};
-	
+
 	var o = "",
 		u = p.url,
 		name = p.name,
@@ -45,13 +48,13 @@ var xpopup = function(p) {
 		l = p.left,
 		t = p.top,
 		r = {};
-	
-	var ap = function(c, x) {
+
+	var ap = function (c, x) {
 		if (o != "") o += ",";
 		o += c + "=" + x;
 	};
-	
-	var pa = function(config, param, def) {
+
+	var pa = function (config, param, def) {
 		if (d(param)) {
 			if ((param === false) || (param === true)) {
 				ap(config, param ? "1" : "0");
@@ -59,22 +62,22 @@ var xpopup = function(p) {
 				ap(config, param);
 			}
 			return param;
-		} else if(d(def)) {
+		} else if (d(def)) {
 			pa(config, def);
 			return def;
 		}
 	};
-	
+
 	// URL
 	if (d(u) && (u != '') && d(p.data)) {
 		// Add params to URL
 		for (var g in p.data) u += ((u.indexOf('?') < 0) ? '?' : '&') + escape(g) + '=' + escape(p.data[g]);
-	}  else if (!d(u)) {
-        u = '';
-    }
-	
+	} else if (!d(u)) {
+		u = '';
+	}
+
 	if (!d(name)) name = 'popup_' + (new Date()).getTime();
-	
+
 	pa("toolbar", prms.toolbar, false);
 	pa("location", prms.location, false);
 	pa("status", prms.status, '');
@@ -84,24 +87,52 @@ var xpopup = function(p) {
 	if (!pa("fullscreen", prms.fullscreen, false)) {
 		if (!d(w)) w = 500;
 		if (!d(h)) h = 400;
-		
-		if (!(d(t) && d(l)) || p.center) {
-			// Center in screen
-			t = parseInt((screen.height / 2) - (h / 2));
-			l = parseInt((screen.width / 2) - (w / 2)); 
+
+		if (!(d(t) && d(l))) {
+			var sh = screen.height,
+				sw = screen.width;
+			if (p.center) {
+				// Center in screen
+				t = parseInt((sh / 2) - (h / 2));
+				l = parseInt((sw / 2) - (w / 2));
+			} else if (p.position) {
+				// Position in screen
+				var c = p.position.toLowerCase().trim().split(/\s+/);
+				// Parent
+				if (c.indexOf('screen') > -1) {
+					sh = window.outerHeight;
+					sw = window.outerWidth;
+				}
+				// Top
+				if (c.indexOf('top') > -1) {
+					t = 0;
+				} else if (c.indexOf('bottom') > -1) {
+					t = sh - h - 100;
+				} else if (c.indexOf('middle') > -1) {
+					t = parseInt((sh / 2) - (h / 2));
+				}
+				// Left
+				if (c.indexOf('left') > -1) {
+					l = 0;
+				} else if (c.indexOf('right') > -1) {
+					l = sw - w - 30;
+				} else if (c.indexOf('center') > -1) {
+					l = parseInt((sw / 2) - (w / 2));
+				}
+			}
 		}
-		
+
 		pa("width", prms.width, w);
 		pa("height", prms.height, h);
 		pa("left", prms.left, l);
 		pa("top", prms.top, t);
 	}
-	
+
 	// Open
 	r.win = window.open(u, name, o);
-	
+
 	// Extended methods
-	r.center = function(){
+	r.center = function () {
 		try {
 			this.win.moveTo; // Check permission
 			var w = this.win,
@@ -121,8 +152,8 @@ var xpopup = function(p) {
 			return false;
 		}
 	};
-	
-	r.maximize = function(){
+
+	r.maximize = function () {
 		try {
 			var w = this.win;
 			w.moveTo(0, 0);
@@ -139,18 +170,18 @@ var xpopup = function(p) {
 			return false;
 		}
 	};
-	
-	r.focus = function(){
+
+	r.focus = function () {
 		this.win.focus();
 	};
-	
-	r.closed = function(){
+
+	r.closed = function () {
 		return this.win.closed;
 	};
-	
+
 	// Modal
 	if (p.modal === true) {
-		var wf = function(e) {
+		var wf = function (e) {
 			if (r.closed()) {
 				window.removeEventListener('click', wf);
 			} else {
@@ -161,26 +192,26 @@ var xpopup = function(p) {
 			}
 		};
 		window.addEventListener('click', wf);
-		
-        var modalw = document.createElement('div');
-        modalw.setAttribute("style", "border:0 none;display:block;height:100%;left:0;margin:0;padding:0;position:absolute;top:0;width:100%;z-index:99999;");
-        modalw.setAttribute("id", "xpopup-modal");
-        document.body.appendChild(modalw);
+
+		var modalw = document.createElement('div');
+		modalw.setAttribute("style", "border:0 none;display:block;height:100%;left:0;margin:0;padding:0;position:absolute;top:0;width:100%;z-index:99999;");
+		modalw.setAttribute("id", "xpopup-modal");
+		document.body.appendChild(modalw);
 	}
-	
+
 	// On close
 	if (d(p.close) || (p.modal === true)) {
 		var cboc = p.close;
-		var wc = setInterval(function(){
+		var wc = setInterval(function () {
 			if (r.closed()) {
 				clearInterval(wc);
 				var e = document.getElementById("xpopup-modal");
-				if (d(e))                 document.body.removeChild(e);
+				if (d(e)) document.body.removeChild(e);
 				if (d(cboc)) cboc();
 			}
 		}, 100);
 	}
-	
+
 	// Return
 	return r;
 };
